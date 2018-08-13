bfe3d155730435fb9296a85ad33ae42066272783
Aleks Saul <aleks.saul@coreos.com>
2018-08-13 16:47:05 -0400

    updates to manifests and script

--- README.md
+++ README.md
@@ -6,6 +6,16 @@ Install kNative airgapped via kustomize
 
 Download [kustomize](https://github.com/kubernetes-sigs/kustomize/releases)
 
+Fetch images, re-tag and push to an internal repo
+
+```sh
+# Set New Repo name in syntax of DOMAIN/REPO
+export repo=docker.io/alekssaul
+./scripts/images.sh
+```
+
+Run Kustomize against istio
+
 ```sh
 kustomize build ./istio/v0.1.0 | kubectl apply -f
 ```
--- scripts/images.sh
+++ scripts/images.sh
@@ -9,7 +9,7 @@ function main {
     istioimages=$(cat $SCRIPTROOT/../istio/$version/base/istio.yaml | grep "image: " | awk -F ': ' '{print $2}' | tr -d '"')
    
     for i in $istioimages; do
-        parse_image $i $repo
+        parse_image $i $repo istio
     done
         
 }
@@ -22,7 +22,7 @@ function parse_image() {
         columncount=$(echo $1 | grep -o ":" | wc -l )
         if [ $columncount -gt 0 ] 
         then
-            fetch_image $1 $2 $slashcount
+            fetch_image $1 $2 $slashcount $4 $version
         fi
     fi
     
@@ -33,6 +33,8 @@ function parse_image() {
 # $1 image to pull
 # $2 new repo base
 # $3 how many slashes ie: docker.io/alpine:latest vs alpine:latest
+# $4 istio or knative
+# $5 version
 function fetch_image() {
     if [ "$3" == "2" ] 
     then
@@ -44,11 +46,22 @@ function fetch_image() {
     fi
 
     echo "Executing: docker pull $1"
-    #docker pull $1    
+    docker pull $1    
     echo "Executing docker tag $1 $newtag"
-    #docker tag $1 $newtag
+    docker tag $1 $newtag
     echo "Executing docker push $newtag"
-    #docker push $newtag
+    docker push $newtag
+    echo "Replacing manifests"
+    
+    case `uname -s` in
+	Darwin)
+        sed -i '' -e 's@'$1'@'$newtag'@g' $SCRIPTROOT/../$4/$5/airgap/*.yaml
+        ;;
+
+	*)
+        sed -i 's@'$1'@'$newtag'@g' $SCRIPTROOT/../$4/$5/airgap/*.yaml
+        ;;
+esac
 }
 
 # --------------------------------------------------------------
